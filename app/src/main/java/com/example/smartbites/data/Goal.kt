3b3a122f421d61b0163600ab702a5725de57ef47
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:08:24 +0200

    Update Goal.kt

--- app/src/main/java/com/example/smartbites/data/Goal.kt
+++ app/src/main/java/com/example/smartbites/data/Goal.kt
@@ -2,6 +2,7 @@
     tableName = "goal",
     foreignKeys = [ForeignKey(entity = User::class, parentColumns = ["userId"], childColumns = ["userId"], onDelete = ForeignKey.CASCADE)]
 )
+
 data class Goal(
     @PrimaryKey(autoGenerate = true) val goalId: Int = 0,
     val userId: Int,
