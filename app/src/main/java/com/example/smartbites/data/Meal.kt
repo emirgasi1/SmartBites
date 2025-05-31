c291032538a6cb2ef3f583f7902a6141e9e24e41
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:08:58 +0200

    Create Meal.kt

---
+++ app/src/main/java/com/example/smartbites/data/Meal.kt
@@ -0,0 +1,11 @@
+@Entity(
+    tableName = "meal",
+    foreignKeys = [ForeignKey(entity = User::class, parentColumns = ["userId"], childColumns = ["userId"], onDelete = ForeignKey.CASCADE)]
+)
+data class Meal(
+    @PrimaryKey(autoGenerate = true) val mealId: Int = 0,
+    val userId: Int,
+    val mealTime: String,
+    val mealType: String,
+    val totalCalories: Int
+)
