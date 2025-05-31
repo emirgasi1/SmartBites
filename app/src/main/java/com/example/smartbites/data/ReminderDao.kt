709aab9d12afc4e4436eb552925d3c0ff7312dfd
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:33:51 +0200

    Create  ReminderDao.kt

---
+++ app/src/main/java/com/example/smartbites/data/ReminderDao.kt
@@ -0,0 +1,11 @@
+@Dao
+interface ReminderDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(reminder: Reminder): Long
+
+    @Query("SELECT * FROM reminder WHERE userId = :userId")
+    fun getRemindersByUser(userId: Int): Flow<List<Reminder>>
+
+    @Delete
+    suspend fun delete(reminder: Reminder)
+}
