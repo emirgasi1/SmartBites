a2c06c8817aef96240948189dadc30b22ccd3460
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:32:14 +0200

    Create GoalDao.kt

---
+++ app/src/main/java/com/example/smartbites/data/GoalDao.kt
@@ -0,0 +1,14 @@
+@Dao
+interface GoalDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(goal: Goal): Long
+
+    @Query("SELECT * FROM goal WHERE userId = :userId")
+    fun getGoalsByUser(userId: Int): Flow<List<Goal>>
+
+    @Update
+    suspend fun update(goal: Goal)
+
+    @Delete
+    suspend fun delete(goal: Goal)
+}
