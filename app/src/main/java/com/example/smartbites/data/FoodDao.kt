018382f89d67162ad316f76c6371d57086cb51cd
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:31:07 +0200

    Create FoodDao.kt

---
+++ app/src/main/java/com/example/smartbites/data/FoodDao.kt
@@ -0,0 +1,14 @@
+@Dao
+interface FoodDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(food: Food): Long
+
+    @Query("SELECT * FROM food")
+    fun getAllFood(): Flow<List<Food>>
+
+    @Update
+    suspend fun update(food: Food)
+
+    @Delete
+    suspend fun delete(food: Food)
+}
