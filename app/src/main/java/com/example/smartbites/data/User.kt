664e6618739e5038c074c54fd1c54d4c749389e4
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:05:57 +0200

    Update User.kt

--- app/src/main/java/com/example/smartbites/data/User.kt
+++ app/src/main/java/com/example/smartbites/data/User.kt
@@ -1,1 +1,11 @@
-
+@Entity(tableName = "user")
+data class User(
+    @PrimaryKey(autoGenerate = true) val userId: Int = 0,
+    val name: String,
+    val email: String,
+    val password: String,
+    val age: Int,
+    val weight: Float,
+    val height: Float,
+    val goalType: String
+)
