a2855cb56f3b40ff6bc4c988c72f5ad465506786
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:08:10 +0200

    Create Food.kt

---
+++ app/src/main/java/com/example/smartbites/data/Food.kt
@@ -0,0 +1,9 @@
+@Entity(tableName = "food")
+data class Food(
+    @PrimaryKey(autoGenerate = true) val foodId: Int = 0,
+    val name: String,
+    val caloriesPer100g: Int,
+    val proteinPer100g: Int,
+    val carbsPer100g: Int,
+    val fatPer100g: Int
+)
