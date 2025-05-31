1ed439d568322f433b2fd9fb72ebddd2456eff26
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:09:34 +0200

    Create MealItem.kt

---
+++ app/src/main/java/com/example/smartbites/data/MealItem.kt
@@ -0,0 +1,13 @@
+@Entity(
+    tableName = "meal_item",
+    primaryKeys = ["mealId", "foodId"],
+    foreignKeys = [
+        ForeignKey(entity = Meal::class, parentColumns = ["mealId"], childColumns = ["mealId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Food::class, parentColumns = ["foodId"], childColumns = ["foodId"], onDelete = ForeignKey.CASCADE)
+    ]
+)
+data class MealItem(
+    val mealId: Int,
+    val foodId: Int,
+    val quantity: Int
+)
