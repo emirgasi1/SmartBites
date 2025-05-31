0dbcd8db86a04ae238d6ef649548081946d42511
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:28:36 +0200

    Update Reminder.kt

--- app/src/main/java/com/example/smartbites/data/Reminder.kt
+++ app/src/main/java/com/example/smartbites/data/Reminder.kt
@@ -2,6 +2,7 @@
     tableName = "reminder",
     foreignKeys = [ForeignKey(entity = User::class, parentColumns = ["userId"], childColumns = ["userId"], onDelete = ForeignKey.CASCADE)]
 )
+
 data class Reminder(
     @PrimaryKey(autoGenerate = true) val reminderId: Int = 0,
     val userId: Int,
