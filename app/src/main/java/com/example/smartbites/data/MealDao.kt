f664fe97e01df7a192389fc542386e6be70a8a34
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:30:32 +0200

    Update MealDao.kt

--- app/src/main/java/com/example/smartbites/data/MealDao.kt
+++ app/src/main/java/com/example/smartbites/data/MealDao.kt
@@ -1,1 +1,14 @@
+@Dao
+interface MealDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(meal: Meal): Long
 
+    @Query("SELECT * FROM meal WHERE userId = :userId")
+    fun getMealsByUser(userId: Int): Flow<List<Meal>>
+
+    @Update
+    suspend fun update(meal: Meal)
+
+    @Delete
+    suspend fun delete(meal: Meal)
+}
