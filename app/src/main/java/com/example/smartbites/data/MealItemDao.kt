938de315f7aa475c9b5c31d19918e0ec7fd2f625
BakirHamzic <161574949+Bakiribu@users.noreply.github.com>
2025-05-31 13:33:04 +0200

    Create MealItemDao.kt

---
+++ app/src/main/java/com/example/smartbites/data/MealItemDao.kt
@@ -0,0 +1,11 @@
+@Dao
+interface MealItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(mealItem: MealItem)
+
+    @Query("SELECT * FROM meal_item WHERE mealId = :mealId")
+    fun getMealItems(mealId: Int): Flow<List<MealItem>>
+
+    @Delete
+    suspend fun delete(mealItem: MealItem)
+}
